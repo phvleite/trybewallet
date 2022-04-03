bd74f540d0e8c6c36a44dca5b22fc0e9e1860ba5
phvleite <phvleite@gmail.com>
2022-04-02 22:09:28 -0300

    requisito 05 finalizado

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,6 +4,18 @@ import { connect } from 'react-redux';
 import { fetchCurrencies } from '../actions';
 
 class Form extends Component {
+  constructor() {
+    super();
+    this.state = {
+      value: 0,
+      currency: '',
+      method: '',
+      tag: '',
+      description: '',
+      exchangeRates: {},
+    };
+  }
+
   componentDidMount() {
     const { getCurrencies } = this.props;
     getCurrencies();
@@ -11,10 +23,79 @@ class Form extends Component {
 
   render() {
     const { currencies } = this.props;
-    console.log(currencies);
+    const { value, description } = this.state;
+    const metodoPagamento = ['Dinheiro', 'Cartão de crédito', 'Cartão de débito', 'Pix'];
+    const categoria = ['Alimentação', 'Lazer', 'Trabalho', 'Transporte', 'Saúde'];
     return (
       <main>
-        Formulário
+        <form className="box-form-expenses" onSubmit={ currencies }>
+          <label htmlFor="value-input" className="label-input">
+            Valor
+            <input
+              type="number"
+              className="value-input"
+              data-testid="value-input"
+              name="valor"
+              value={ value }
+              onChange={ this.onInputChange }
+              id="value-input"
+            />
+          </label>
+          <label htmlFor="currency-input" className="label-input">
+            Moeda
+            <select
+              data-testid="currency-input"
+              id="currency-input"
+              className="select-input"
+            >
+              { currencies.map((currency, ind) => (
+                <option key={ ind } value={ currency }>{ currency }</option>
+              ))}
+            </select>
+          </label>
+          <label htmlFor="method-input" className="label-input">
+            Método de pagamento
+            <select
+              data-testid="method-input"
+              id="method-input"
+              className="select-input"
+            >
+              { metodoPagamento.map((currency, ind) => (
+                <option key={ ind } value={ currency }>{ currency }</option>
+              ))}
+            </select>
+          </label>
+          <label htmlFor="tag-input" className="label-input">
+            Método de pagamento
+            <select
+              data-testid="tag-input"
+              id="tag-input"
+              className="select-input"
+            >
+              { categoria.map((currency, ind) => (
+                <option key={ ind } value={ currency }>{ currency }</option>
+              ))}
+            </select>
+          </label>
+          <label htmlFor="description-input" className="label-input">
+            Valor
+            <input
+              type="text"
+              className="description-input"
+              data-testid="description-input"
+              name="descricao"
+              value={ description }
+              onChange={ this.onInputChange }
+              id="description-input"
+            />
+          </label>
+          <input
+            type="submit"
+            // className="btn-input"
+            // disabled={ isDisabled }
+            value="Adcionar despesa"
+          />
+        </form>
       </main>
     );
   }
