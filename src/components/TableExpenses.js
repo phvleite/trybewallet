1f79a716dba5559c7160dd653f8ca08978ea8784
phvleite <phvleite@gmail.com>
2022-04-05 14:15:12 -0300

    requisito 8 finalizado

--- src/components/TableExpenses.js
+++ src/components/TableExpenses.js
@@ -5,12 +5,12 @@ import '../css/TableExpenses.css';
 
 class TableExpenses extends Component {
   renderTableHeader() {
-    // Descrição, Tag, Método de pagamento, Valor, Moeda, Câmbio utilizado, Valor convertido, Moeda de conversão e Editar/Excluir
     return (
-      <tr>
+      <tr className="table-header">
         <th>Descrição</th>
         <th>Tag</th>
         <th>Método de pagamento</th>
+        <th>Valor</th>
         <th>Moeda</th>
         <th>Câmbio utilizado</th>
         <th>Valor convertido</th>
@@ -22,12 +22,33 @@ class TableExpenses extends Component {
 
   render() {
     const { expenses } = this.props;
-    console.log(expenses);
+    const dbExchangeRates = expenses
+      .map((expense) => expense.exchangeRates[expense.currency]);
+    const dbCurrency = dbExchangeRates.map((currency) => currency.name.split('/'));
+    const decimals = 2;
     return (
       <section className="table-expenses">
         <table id="expenses">
           <tbody>
             { this.renderTableHeader() }
+            { (expenses.length > 0) && expenses.map((expense, ind) => (
+              <tr key={ expense.id } className="table-body">
+                <td>{ expense.description }</td>
+                <td>{ expense.tag }</td>
+                <td>{ expense.method }</td>
+                <td>{ parseFloat(expense.value).toFixed(2) }</td>
+                <td>
+                  { dbCurrency[ind][0] === 'Dólar Americano'
+                    ? 'Dólar Comercial'
+                    : dbCurrency[ind][0] }
+                </td>
+                <td>{ parseFloat(dbExchangeRates[ind].ask).toFixed(decimals) }</td>
+                <td>
+                  { parseFloat(expense.value * dbExchangeRates[ind].ask).toFixed(2) }
+                </td>
+                <td>Real</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </section>);
